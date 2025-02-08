58c6488450a6d2ea105db63bb9501359978af93f
ayushgupta9906 <ayush150152@gmail.com>
2025-02-08 18:11:25 +0530

    Fix missing CSS import

--- src/components/forms/OptionForm.component.jsx
+++ src/components/forms/OptionForm.component.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import './OptionForm.css'; // Import your CSS file
+import './OptionForm.css';
 
 function OptionForm({ setSelectedOption }) {
     const [selected, setSelected] = useState('t');
